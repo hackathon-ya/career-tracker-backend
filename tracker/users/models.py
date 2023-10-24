261db4ea3fec094c9fbfabb25b5ac6a158df9b3f
Михаил Францкевич <ogurets13@gmail.com>
2023-10-25 03:42:30 +0400

    users fixed again

--- tracker/users/models.py
+++ tracker/users/models.py
@@ -40,7 +40,9 @@ class Candidate(AbstractUser):
         related_name="candidates",
         verbose_name="Город",
     )
-    date_of_birth = models.DateField(null=True, blank=True, verbose_name="Дата рождения")
+    date_of_birth = models.DateField(
+        null=True, blank=True, verbose_name="Дата рождения"
+    )
     status_from_kt = models.ForeignKey(
         Status_from_kt,
         on_delete=models.SET_NULL,
@@ -49,38 +51,78 @@ class Candidate(AbstractUser):
         verbose_name="Статус в карьерном треке",
     )
     form_of_employment = models.ManyToManyField(
-        Forms_of_employment, related_name="candidate_employments", verbose_name="Тип занятости"
+        Forms_of_employment,
+        related_name="candidate_employments",
+        verbose_name="Тип занятости",
     )
     work_arrangement = models.ManyToManyField(
         Work_arrangements, related_name="candidate_works", verbose_name="Формат работы"
     )
-    active = models.BooleanField(verbose_name="Активный пользователь", null=True, blank=True,)
-    last_activity = models.DateTimeField(verbose_name="Был активен", null=True, blank=True,)
-    experience_months = models.IntegerField(verbose_name="Опыт работы", null=True, blank=True,)
+    active = models.BooleanField(
+        verbose_name="Активный пользователь",
+        null=True,
+        blank=True,
+    )
+    last_activity = models.DateTimeField(
+        verbose_name="Был активен",
+        null=True,
+        blank=True,
+    )
+    experience_months = models.IntegerField(
+        verbose_name="Опыт работы",
+        null=True,
+        blank=True,
+    )
     education = models.ForeignKey(
         Education,
         on_delete=models.SET_NULL,
-        null=True, blank=True,
+        null=True,
+        blank=True,
         related_name="candidates",
         verbose_name="Образование",
     )
     education_YP = models.ForeignKey(
         Education_YP,
         on_delete=models.SET_NULL,
-        null=True, blank=True,
+        null=True,
+        blank=True,
         related_name="candidates",
         verbose_name="Курс в Яндекс Практикуме",
     )
-    resume = models.FileField(upload_to="resumes/", verbose_name="Резюме", null=True, blank=True,)
-    skills = models.ManyToManyField(
-        Skills, related_name="candidate_skills", verbose_name="Навыки",
+    resume = models.FileField(
+        upload_to="resumes/",
+        verbose_name="Резюме",
+        null=True,
+        blank=True,
     )
-    mobile = models.CharField(max_length=16, verbose_name="Телефон", null=True, blank=True,)
-    telegram = models.CharField(max_length=255, verbose_name="Телеграм", null=True, blank=True,)
-    pub_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата размещения", null=True, blank=True,)
+    skills = models.ManyToManyField(
+        Skills,
+        related_name="candidate_skills",
+        verbose_name="Навыки",
+    )
+    mobile = models.CharField(
+        max_length=16,
+        verbose_name="Телефон",
+        null=True,
+        blank=True,
+    )
+    telegram = models.CharField(
+        max_length=255,
+        verbose_name="Телеграм",
+        null=True,
+        blank=True,
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата размещения",
+        null=True,
+        blank=True,
+    )
 
-    groups = models.ManyToManyField(Group, related_name='candidate_groups')
-    user_permissions = models.ManyToManyField(Permission, related_name='candidate_user_permissions')
+    groups = models.ManyToManyField(Group, related_name="candidate_groups")
+    user_permissions = models.ManyToManyField(
+        Permission, related_name="candidate_user_permissions"
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
