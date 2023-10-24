8ca55cd43bab80e0a01dbc01395678068c6f4ebe
Михаил Францкевич <ogurets13@gmail.com>
2023-10-24 16:33:39 +0400

    user models + docstrings

--- tracker/users/models.py
+++ tracker/users/models.py
@@ -3,20 +3,39 @@ from django.db import models
 
 
 class Recruiter(AbstractUser):
+    """
+    Рекрутер, основной пользователь, через которого осуществляется
+    авторизация и взаимодействие с вакансиями и соискателями
+    """
+
     company = models.CharField(max_length=200)
     company_inn = models.CharField(max_length=12)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Рекрутер"
+        verbose_name_plural = "Рекрутеры"
 
 
 class Candidate(AbstractUser):
+    """
+    Соискатель, загружается в базу данных на основе данных из карьерного трекера
+    """
+
     city = models.ForeignKey(
-        "City", on_delete=models.SET_NULL, related_name="candidates", verbose_name="Город"
+        "City",
+        on_delete=models.SET_NULL,
+        related_name="candidates",
+        verbose_name="Город",
     )
     date_of_birth = models.DateField(verbose_name="Дата рождения")
     status_from_kt = models.ForeignKey(
-        "Status_from_kt", on_delete=models.SET_NULL, related_name="candidates", verbose_name="Статус в карьерном треке"
+        "Status_from_kt",
+        on_delete=models.SET_NULL,
+        related_name="candidates",
+        verbose_name="Статус в карьерном треке",
     )
     form_of_employment = models.ManyToManyField(
         "Forms_of_employment", related_name="candidates", verbose_name="Тип занятости"
@@ -28,22 +47,38 @@ class Candidate(AbstractUser):
     last_activity = models.DateTimeField(verbose_name="Был активен")
     experience_months = models.IntegerField(verbose_name="Опыт работы")
     education = models.ForeignKey(
-        "Education", on_delete=models.SET_NULL, related_name="candidates", verbose_name="Образование"
+        "Education",
+        on_delete=models.SET_NULL,
+        related_name="candidates",
+        verbose_name="Образование",
     )
     education_YP = models.ForeignKey(
-        "Education_YP", on_delete=models.SET_NULL, related_name="candidates", verbose_name="Курс в Яндекс Практикуме"
+        "Education_YP",
+        on_delete=models.SET_NULL,
+        related_name="candidates",
+        verbose_name="Курс в Яндекс Практикуме",
     )
     resume = models.FileField(upload_to="resumes/", verbose_name="Резюме")
-    skills = models.ManyToManyField("Skills", related_name="candidates", verbose_name="Навыки")
+    skills = models.ManyToManyField(
+        "Skills", related_name="candidates", verbose_name="Навыки"
+    )
     mobile = models.CharField(max_length=16, verbose_name="Телефон")
     telegram = models.CharField(verbose_name="Телеграм")
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата размещения")
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Соискатель"
+        verbose_name_plural = "Соискатели"
 
 
 class Favorites(models.Model):
+    """
+    Модель-посредник для добавление кандидатов в избранное к рекрутерам
+    """
+
     candidate = models.ForeignKey(
         Candidate, on_delete=models.CASCADE, related_name="favorited_by"
     )
