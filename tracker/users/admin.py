4bd160e56defbb3e9259c5422fdd0699204ca8e4
Михаил Францкевич <ogurets13@gmail.com>
2023-10-25 04:06:07 +0400

    rating added

--- tracker/users/admin.py
+++ tracker/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Recruiter, Candidate, Favorites
+from .models import Recruiter, Candidate, Favorites, Ratings
 
 
 @admin.register(Recruiter)
@@ -14,9 +14,9 @@ class RecruiterAdmin(admin.ModelAdmin):
         "email",
     )
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
-        ('Company', {'fields': ('company', 'company_inn')}),
+        (None, {"fields": ("username", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "email")}),
+        ("Company", {"fields": ("company", "company_inn")}),
     )
 
 
@@ -31,17 +31,25 @@ class CandidateAdmin(admin.ModelAdmin):
         "active",
     )
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'city')}),
-        ('Status', {'fields': ('status_from_kt', 'active', 'last_activity')}),
-        ('Employment', {'fields': ('form_of_employment', 'work_arrangement')}),
-        ('Education', {'fields': ('education', 'education_YP')}),
-        ('Skills', {'fields': ('skills',)}),
-        ('Contact Info', {'fields': ('mobile', 'telegram')}),
-        ('Permissions', {'fields': ('groups', 'user_permissions')}),
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal Info",
+            {"fields": ("first_name", "last_name", "email", "date_of_birth", "city")},
+        ),
+        ("Status", {"fields": ("status_from_kt", "active", "last_activity")}),
+        ("Employment", {"fields": ("form_of_employment", "work_arrangement")}),
+        ("Education", {"fields": ("education", "education_YP")}),
+        ("Skills", {"fields": ("skills",)}),
+        ("Contact Info", {"fields": ("mobile", "telegram")}),
+        ("Permissions", {"fields": ("groups", "user_permissions")}),
     )
 
 
 @admin.register(Favorites)
 class FavoritesAdmin(admin.ModelAdmin):
     list_display = ("candidate", "recruiter")
+
+
+@admin.register(Ratings)
+class RatingsAdmin(admin.ModelAdmin):
+    list_display = ("candidate", "recruiter", "rating")
