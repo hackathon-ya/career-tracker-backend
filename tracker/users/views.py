e7f56e45c5675dc9eb8df4173d9a5437fe72ada2
Mikhail Frantskevich <ogurets13@gmail.com>
2023-10-25 23:26:29 +0400

    minor fix

--- tracker/users/views.py
+++ tracker/users/views.py
@@ -17,9 +17,3 @@ class CandidateViewSet(ListRetrieveViewset):
     serializer_class = CandidateSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = CandidateFilter
-    filterset_fields = {
-        "city": ["exact"],  # Filtering by exact match
-        "education": ["exact"],
-        "education_YP": ["exact"],
-        "status_from_kt": ["exact"],
-    }
