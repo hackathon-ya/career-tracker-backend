6bb549808a7a79384b5b7098b21b2acfcf94e89b
Dmitriy Ushakov Air <udv8@ya.ru>
2023-10-28 18:25:11 +0300

    minor fix

--- tracker/tracker/settings.py
+++ tracker/tracker/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "careertracker_backend:8000", "192.168.65.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 
 DJANGO_APPS = [
     "django.contrib.admin",
